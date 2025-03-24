d0c9aae4a5daa5f3760aa5f73da867fad606cd2a
brymmobaggins <ibro2tablet21@yahoo.com>
2025-03-24 16:34:32 +0100

    create  sign up pages

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <div>EduConvert</div>
+      <div>Create New Account</div>
+      <div>
+        <button>FaceBook</button>
+        <button>Google</button>
+      </div>
+      <div>
+        <label>Name</label>
+        <input type="text" value="" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="Email" value="" />  
+        
+      </div>
+      <div className="">
+        <label htmlFor="">Password</label> 
+        <input type="password" value="" />
+      </div>
+    </>
+  );
+}
