bd9c49c4377ac2694de77fd376e05c754005bc51
brymmobaggins <ibro2tablet21@yahoo.com>
2025-03-31 20:02:12 +0100

    style resgiter page

--- src/components/UpLoadSection.jsx
+++ src/components/UpLoadSection.jsx
@@ -1,10 +1,11 @@
 /** @format */
+import { Link } from "react-router-dom";
 
 export default function UpLoadSection() {
   return (
     <>
       <section className="flex flex-row justify-between gap-4 m-auto w-[85%] mt-10 p-5 border border-gray-200 rounded-lg shadow-md *:border *:hover:cursor-pointer *:p-5">
-        <div className="duration-300 ease-in" id="first-card">
+        <div className="transition duration-300 ease-in" id="first-card">
           <h1>PDF to Word</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
@@ -32,6 +33,11 @@ export default function UpLoadSection() {
           </p>
         </div>
         <div className="" id="second-card">
+          <div className="text-right">
+            <Link className="border border-[var(--warning-color)] rounded p-1 mr-0">
+              Coming Soon
+            </Link>
+          </div>
           <h1>PDF to PowerPoint</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,29 +1,71 @@
 /** @format */
 
-;
-
 export default function SignUpPage() {
   return (
     <>
-      <div>EduConvert</div>
-      <div>Create New Account</div>
-      <div> 
-        <button>FaceBook</button>
-        <button>Google</button>
-      </div>
-      <div>
-        <label>Name</label>
-        <input type="text" value="" />
-      </div>
-      <div>
-        <label>Email</label>
-        <input type="Email" value="" />  
- 
-      </div>
-      <div className="">
-        <label htmlFor="">Password</label> 
-        <input type="password" value="" />
-      </div>
+      <section className="flex min-h-[100vh]  ">
+        {/* Left Column */}
+        <div
+          className="flex-6 flex-col justify-center border p-[80px] bg-white text-black"
+          id="left-column"
+        >
+          <h1 className="text-center text-5xl font-bold">ConvertZilla</h1>
+          <div className="text-center text-3xl font-bold my-5">
+            Create new account
+          </div>
+          <div
+            className="flex justify-between items-center max-w-[400px] 
+          m-auto mt-2 py-3 *:px-5 *:py-1.5 *:rounded *:border *:font-bold"
+          >
+            <a href="">Facebook</a>
+            <a href="">Google</a>
+            <a href="">SS0</a>
+          </div>
+          <div>
+            <input type="text" placeholder="Enter your name" />
+          </div>
+
+          <div className="">
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div>
+            <input type="password" placeholder="Password" />
+          </div>
+
+          
+          <div className="text-center mt-3 cursor-pointer">
+            <button className="text-[var(--color-dark-blue)] bg-[var(--color-neon-green)] hover:bg-[var(--color-neon-green-500)] font-bold cursor-pointer outline-0 rounded px-5 py-1.5">
+              Sign Up
+            </button>
+          </div>
+
+          <p className="text-center mt-3">
+            Already a Member?{" "}
+            <a
+              href=""
+              className="underline decoration-[var(--danger-color)] text-[var(--danger-color)] font-bold"
+            >
+              Login
+            </a>
+          </p>
+        </div>
+        <div></div>
+        <div className="flex-4 flex-col justify-center p-[60px] border">
+          {/* right column */}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo quos
+          qui possimus magnam velit? Nesciunt consequuntur aperiam impedit
+          molestias accusamus rem et, doloremque delectus ducimus minima
+          recusandae repellat, qui eos quam nostrum nulla aliquid,
+          exercitationem iure similique veniam ratione omnis est nemo! Assumenda
+          debitis earum doloribus, quisquam consectetur delectus tempore enim
+          fugiat repellendus, itaque accusantium? Omnis similique eos aliquid
+          doloribus! Eveniet molestiae magnam, labore temporibus laudantium odit
+          doloribus minus totam, quos explicabo officia, aut possimus delectus
+          id recusandae? Iusto excepturi eveniet sit dolores architecto labore
+          assumenda suscipit ipsa numquam quas, molestiae molestias esse nemo,
+          quisquam omnis soluta dicta aliquid. Temporibus.
+        </div>
+      </section>
     </>
   );
 }
