de6f3146740d4c1d3346c81f9a4f7310bc144440
brymmobaggins <ibro2tablet21@yahoo.com>
2025-06-27 17:30:56 +0100

    add login script for dummy users

--- js/login.js
+++ js/login.js
@@ -1,5 +1,11 @@
 /** @format */
 
+const adminUser = {
+  username: "admin",
+  password: "1234",
+  role: "admin",
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const title = document.getElementById("loginTitle");
   const form = document.getElementById("loginForm");
@@ -8,20 +14,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Adjust title
   title.textContent = role === "admin" ? "Admin Login" : "Student Login";
-
   typeText.textContent =
     role === "admin"
       ? "You're logging in as an Admin"
       : "You're logging in as a Student";
 
   // Handle form submit
+  // Handle form submit
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    if (role === "admin") {
+    const username = document.getElementById("user-input").value;
+    const password = document.getElementById("user-password").value;
+
+    if (username == adminUser.username && password === adminUser.password) {
+      const user = JSON.parse(localStorage.getItem("user")) || [];
+      // Move the declaration of dummyUsers here, before it's used
+      const dummyUsers = {
+        username,
+        password,
+      };
+      dummyUsers.push(user);
+
+      localStorage.setItem(JSON.stringify(user));
+
       window.location.href = "admin-dashboard.html";
     } else {
-      window.location.href = "student-dashboard.html";
+      alert("invalid Credentials");
     }
+
+    // if (role === "admin") {
+    //   window.location.href = "admin-dashboard.html";
+    // } else {
+    //   window.location.href = "student-dashboard.html";
+    // }
   });
 });
