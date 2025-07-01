1edba4c8edaee5723f1bb51d4a21116b2ec0c195
brymmobaggins <ibro2tablet21@yahoo.com>
2025-07-01 13:16:26 +0100

    feat: add logout functionality to admin dashboard (v1.1.2)

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -5,3 +5,10 @@ const user = JSON.parse(localStorage.getItem("loggedInUser"));
 if (!user || !user.role == "admin") {
   window.location.href = "login.html";
 }
+
+const logoutBtn = document.getElementById("log-out");
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUser");
+  window.location.href = "index.html";
+});
