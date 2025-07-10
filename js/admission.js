61cb9cfdce26299c6aafed9262c64dd5beb5f6a7
brymmobaggins <ibro2tablet21@yahoo.com>
2025-07-10 09:13:00 +0100

    import stacks via npm and remove the CDN

--- js/admission.js
+++ js/admission.js
@@ -1,5 +1,18 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("admission-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent form submission
+
+    const formData = new FormData(form)
+
+    const studentData = {
+        fullname : formData.get("fullName")
+    }
 
 
 
-
-function
+  });
+});
