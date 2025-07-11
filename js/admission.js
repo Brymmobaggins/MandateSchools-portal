839598e6a32658fbc87c39919b2632ebd5edfe90
brymmobaggins <ibro2tablet21@yahoo.com>
2025-07-11 10:15:15 +0100

    feat(admission): add admission and local storage submission (v1.3.0)

--- js/admission.js
+++ js/admission.js
@@ -1,4 +1,4 @@
-
+/** @format */
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("admission-form");
@@ -9,9 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const formData = new FormData(form);
 
     const studentData = {
-      fullname: formData.get("fullName")
-
+      fullname: formData.get("fullName"),
+      email: formData.get("email"),
     };
 
+    // get the item from local storage, if it is not available assign an empty array to it
+    const application =
+      JSON.parse(localStorage.getItem("admissionApplication")) || [];
+
+    // push or add new applicant to the array
+    application.push(studentData);
+
+    // save back to local storage
+    localStorage.setItem("admissionApplication", JSON.stringify(application));
+
+
+    // 
+    alert("Application Submitted Successfully")
+
+    // reset form
+    form.reset();
   });
 });
